cb59ae58c48747292f93a8309feea179cbd43ccd
Kobby43 <fantwiasiedu@gmail.com>
2022-07-04 09:46:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /** 
  * main - prints the phrase "Programming is like a multilingual puzzle"
  * Return: 0 if exited properly, non-zero otherwise
- **/
+ */
 
 int main(void)
 { 
 
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	
-return (0);
+	return (0);
 }
