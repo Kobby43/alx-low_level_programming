bb34f7b56bb4427d06942f6a60650fb29a9918a9
Kobby43 <fantwiasiedu@gmail.com>
2022-07-13 06:19:03 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,25 +8,25 @@ int main(void)
 {
 	int i, j;
 
-		for (i = 0; j < 100; i++)
-		{
-			for (j = 0; j < 100; j++)
+			for (i = 0; j < 100; i++)
 			{
-				if (i < j)
+				for (j = 0; j < 100; j++)
 				{
-					putchar((i / 10) + 48);
-					putchar((i % 10) + 48);
-					putchar(' ');
-					putchar((j / 10) + 48);
-					putchar((j % 10) + 48);
-					if (i != 98 || j != 99)
+					if (i < j)
 					{
-						putchar(',');
+						putchar((i / 10) + 48);
+						putchar((i % 10) + 48);
 						putchar(' ');
+						putchar((j / 10) + 48);
+						putchar((j % 10) + 48);
+						if (i != 98 || j != 99)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
 			}
-		}
-		putchar('\n');
-		return (0);
+			putchar('\n');
+			return (0);
 }
