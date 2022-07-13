fa4eb3231f96b3cd3a35af745364a1d07b24bbd1
Kobby43 <fantwiasiedu@gmail.com>
2022-07-13 08:01:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	_putchar('M')
-	_putchar('a')
-	_putchar('i')
-	_putchar('n')
-	_putchar('\n')
+	_putchar('M');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+	_putchar('\n');
 
 	return (0);
 }
